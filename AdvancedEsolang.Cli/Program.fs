d7c461ab871ce4010f8bf5973454c23d470474c2
Ketok <ketok4321@gmail.com>
2023-10-21 20:39:28 +0200

    Replace Argu with System.CommandLine to allow for AOT compilation

--- AdvancedEsolang.Cli/Program.fs
+++ AdvancedEsolang.Cli/Program.fs
@@ -1,7 +1,7 @@
 ﻿open System.IO
 
+open System.CommandLine
 open FParsec
-open Argu
 
 open AdvancedEsolang.Syntax
 open AdvancedEsolang.InterpreterCS
@@ -38,126 +38,90 @@ let read (path: string): Library =
     let fpath = path |> Path.GetFullPath
     readD (fpath |> Path.GetDirectoryName) fpath depCache
 
-[<CliPrefix(CliPrefix.Dash)>]
-type RunArgs =
-    | [<MainCommand; ExactlyOnce>] Path of string
-
-    interface IArgParserTemplate with
-        member this.Usage =
-            match this with
-            | Path _ -> "path of the file to run."
-
-and FormatArgs =
-    | [<MainCommand; ExactlyOnce>] Path of string
-    
-    interface IArgParserTemplate with
-        member this.Usage =
-            match this with
-            | Path _ -> "path of the file to format."
-
-and GenerateArgs =
-    | [<MainCommand; ExactlyOnce>] Name of string
-    | [<AltCommandLine("-c"); Unique>] Count of int
-    | [<AltCommandLine("-o"); Unique>] Output of string
-
-    interface IArgParserTemplate with
-        member this.Usage =
-            match this with
-            | Name _ -> "name of the generator."
-            | Count _ -> "count of things to generate passed to the generator."
-            | Output _ -> "path of the output file."
-
-and MergeArgs =
-    | [<MainCommand; ExactlyOnce>] Path of string
-    | [<AltCommandLine("-o"); Unique>] Output of string
-
-    interface IArgParserTemplate with
-        member this.Usage =
-            match this with
-            | Path _ -> "path of the file to merge."
-            | Output _ -> "path of the output file."
-
-type CliArguments =
-    | [<AltCommandLine("-v")>] Version
-    | [<CliPrefix(CliPrefix.None)>] Run of ParseResults<RunArgs>
-    | [<CliPrefix(CliPrefix.None)>] Format of ParseResults<FormatArgs>
-    | [<CliPrefix(CliPrefix.None); AltCommandLine("gen")>] Generate of ParseResults<GenerateArgs>
-    | [<CliPrefix(CliPrefix.None)>] Merge of ParseResults<MergeArgs>
-    
-    interface IArgParserTemplate with
-        member this.Usage =
-            match this with
-            | Version -> "print the version of the interpreter."
-            | Run _ -> "run the specified file."
-            | Format _ -> "format the specified file."
-            | Generate _ -> "generate a library."
-            | Merge _ -> "merge a file with all its imports outputting a single-file program."
-
 [<EntryPoint>]
 let main argv =   
-    let parser = ArgumentParser.Create<CliArguments>()
+    let rootCmd = RootCommand()
 
+    let input = Argument<string>("input", "path of the input file.")    
+    let output = Option<string>([|"--output"; "-o"|], "path of the output file.")
+    
+    let libName = Argument<string>("name", "name of the library to generate.")
+    let number = Option<int>([|"--number"; "-n"|], "number passed as an argument to the generator.")
+    
+    let runCmd = Command("run", "run the specified file.")
+    runCmd.AddArgument(input)
+    
+    let formatCmd = Command("format", "format the specified file.")
+    formatCmd.AddArgument(input)
+    
+    let generateCmd = Command("generate", "generate a library.")
+    generateCmd.AddAlias("gen")
+    generateCmd.AddArgument(libName)
+    generateCmd.AddOption(output)
+    generateCmd.AddOption(number)
+    
+    let mergeCmd = Command("merge", "merge a file with all its imports outputting a single-file program.")
+    mergeCmd.AddArgument(input)
+    mergeCmd.AddOption(output)
+    
+    for c in [runCmd; formatCmd; generateCmd; mergeCmd] do rootCmd.AddCommand(c)    
+    
+    runCmd.SetHandler(fun input ->
+        let evalParser = System.Func<string, Statement seq>(fun text ->
+            match runParserOnString Parsers.stmts BuiltinTypes.library "eval" (text + "\nend") with
+            | Success (res, _, _) ->
+                res
+            | Failure (message, error, _) -> failwith message
+        )
+        
+        let interpreter = AdvInterpreter(read input, evalParser)
+        BuiltinMethods.AddAll(interpreter)
+        interpreter.Run()
+    , input)
+    
+    formatCmd.SetHandler(fun input ->
+        let library = read input
+        let code = Stringifier.sLibrary library
+        File.WriteAllText(input, code)
+    , input)
+    
+    generateCmd.SetHandler(fun name output number ->
+        let watch = System.Diagnostics.Stopwatch()
+        watch.Start()
+
+        printfn "Generating..."
+        let library =
+            if name = "builtin" then
+                BuiltinTypes.library
+            else
+                match generators.TryGetValue(name) with
+                | true, gen -> gen number
+                | false, _ -> failwithf "Unknown generator '%s'." name
+        
+        printfn "Generated in %i ms." watch.ElapsedMilliseconds
+        watch.Restart()
+
+        printfn "Stringifying..."
+        let libraryStr = Stringifier.sLibrary library
+        printfn "Stringified in %i ms." watch.ElapsedMilliseconds
+        watch.Restart()
+
+        printfn "Saving..."
+        File.WriteAllText (output, libraryStr)
+        printfn "Saved in %i ms." watch.ElapsedMilliseconds
+        watch.Restart()
+    , libName, output, number)
+    
+    mergeCmd.SetHandler(fun input output ->
+        let lib = read input
+
+        let merged = { lib with dependencies = [BuiltinTypes.library]; classes = (lib.fullDeps |> List.filter (fun d -> d <> BuiltinTypes.library) |> List.rev |> List.map (fun l -> l.classes) |> List.concat) }
+        let code = Stringifier.sLibrary merged
+        File.WriteAllText(output, code)
+    , input, output)
+    
     try
-        let result = parser.ParseCommandLine(inputs = argv, raiseOnUsage = true)
-
-        match result.GetSubCommand() with
-        | Version -> printfn "%s" (System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString())
-        | Run r ->
-            let evalParser = System.Func<string, Statement seq>(fun text ->
-                match runParserOnString Parsers.stmts BuiltinTypes.library "eval" (text + "\nend") with
-                | Success (res, _, _) ->
-                    res
-                | Failure (message, error, _) -> failwith message
-            )
-            
-            let interpreter = AdvInterpreter(read (r.GetResult(RunArgs.Path)), evalParser)
-            BuiltinMethods.AddAll(interpreter)
-            interpreter.Run()
-        | Format r ->
-            let path = r.GetResult(FormatArgs.Path)
-            
-            let library = read path
-            let code = Stringifier.sLibrary library
-            File.WriteAllText(path, code)
-        | Generate r ->
-            let name = r.GetResult(Name)
-            
-            let watch = System.Diagnostics.Stopwatch()
-            watch.Start()
-
-            printfn "Generating..."
-            let library =
-                if name = "builtin" then
-                    BuiltinTypes.library
-                else
-                    let count = r.GetResult(Count)
-
-                    match generators.TryGetValue(name) with
-                    | true, gen -> gen count
-                    | false, _ -> failwithf "Unknown generator '%s'." name
-            
-            printfn "Generated in %i ms." watch.ElapsedMilliseconds
-            watch.Restart()
-
-            printfn "Stringifying..."
-            let libraryStr = Stringifier.sLibrary library
-            printfn "Stringified in %i ms." watch.ElapsedMilliseconds
-            watch.Restart()
-
-            printfn "Saving..."
-            File.WriteAllText (r.GetResult(GenerateArgs.Output, defaultValue = $"./{name}.adv"), libraryStr)
-            printfn "Saved in %i ms." watch.ElapsedMilliseconds
-            watch.Restart()
-        | Merge r ->
-            let path = r.GetResult(MergeArgs.Path)
-            let output = r.GetResult(MergeArgs.Output)
-
-            let lib = read path
-
-            let merged = { lib with dependencies = [BuiltinTypes.library]; classes = (lib.fullDeps |> List.filter (fun d -> d <> BuiltinTypes.library) |> List.rev |> List.map (fun l -> l.classes) |> List.concat) }
-            
-            File.WriteAllText(output, Stringifier.sLibrary merged)
-        0
+        rootCmd.Invoke(argv)
     with
     | :? AdvException as err ->
         eprintfn "%s" err.Message
